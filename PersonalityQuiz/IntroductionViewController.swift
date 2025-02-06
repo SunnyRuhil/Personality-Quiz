890fbfccc1d8f6d43fcef7008dff6347532c92f0
SunnyRuhil <143391195+SunnyRuhil@users.noreply.github.com>
2025-02-07 01:00:28 +0530

    Add files via upload

---
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Sunny on 15/09/2024
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
